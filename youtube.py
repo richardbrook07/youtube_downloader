a6505369cd9156ebd5f3f0e871de749812a40e3c
Anirban <richardbrook07@gmail.com>
2022-01-06 01:38:27 +0530

    changed youtube.py

--- youtube.py
+++ youtube.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 import pytube
 try:
-    video_url = 'https://www.youtube.com/watch?v=-SlFVwcuUG8'
+    video_url = 'https://www.youtube.com/watch?v=mUs97qXjw1M'
     youtube = pytube.YouTube(video_url)
     video = youtube.streams.first()
     video.download('E:\youtube-download')
