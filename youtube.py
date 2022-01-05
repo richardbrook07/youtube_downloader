03bbe5428b79e20720f139fa8b38c8a743b5b237
Anirban <richardbrook07@gmail.com>
2022-01-06 01:26:09 +0530

    Initial commit

---
+++ youtube.py
@@ -0,0 +1,10 @@
+from pytube import YouTube
+import pytube
+try:
+    video_url = 'https://www.youtube.com/watch?v=-SlFVwcuUG8'
+    youtube = pytube.YouTube(video_url)
+    video = youtube.streams.first()
+    video.download('E:\youtube-download')
+    print("Download Successfull !!")
+except:
+    print("Something Went Wrong !!")
